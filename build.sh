d943648143eba991a5220abd49bddb673d87b9be
VitorJonatasPB <vitor.jonatas98@gmail.com>
2026-03-09 21:39:23 -0400

    feat: Adiciona a criação automática de superusuário Django ao script de build.

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ pip install -r backend/requirements.txt
 cd backend
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; import os; User = get_user_model(); User.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME')).exists() or User.objects.create_superuser(os.environ.get('DJANGO_SUPERUSER_USERNAME'), os.environ.get('DJANGO_SUPERUSER_EMAIL'), os.environ.get('DJANGO_SUPERUSER_PASSWORD'))"
